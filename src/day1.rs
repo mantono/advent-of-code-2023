50e191d056bff78e15bdbe5bad7093dc85308845
Anton Österberg <anton.osterberg@protonmail.com>
2023-12-05 00:26:55 +0100

    wip: Day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use aoc::Solver;
 
 pub struct Aoc;
@@ -6,12 +8,37 @@ impl Solver for Aoc {
     // Change the type of Item to the one that will be processed by the solver
     type Item = String;
 
-    fn solve_one(_inputs: &[Self::Item]) -> String {
-        "This is the answer of the first part of the puzzle".to_string()
+    fn puzzle() -> aoc::Puzzle {
+        aoc::Puzzle::new(2023, 1)
     }
 
-    // Remove these commments when solving part 2!
-    // fn solve_two(inputs: &[Self::Item]) -> String {
-    //     "This is the answer of the second part of the puzzle".to_string()
-    // }
+    fn solve_one(inputs: &[Self::Item]) -> String {
+        let sum: usize = inputs
+            .iter()
+            .map(|line| {
+                line.as_str()
+                    .chars()
+                    .filter(|c| c.is_ascii_digit())
+                    //.map(|c| usize::from_str(c.to_string().as_str()).unwrap())
+                    .collect()
+            })
+            .map(|digits: Vec<char>| {
+                format!("{}{}", digits.first().unwrap(), digits.last().unwrap())
+            })
+            .map(|s: String| usize::from_str(&s).unwrap())
+            .sum();
+
+        sum.to_string()
+    }
+
+    fn solve_two(inputs: &[Self::Item]) -> String {
+        "todo".to_string()
+    }
+}
+
+fn find_num(line: &str) -> Vec<usize> {
+    let mut found: Vec<(usize, usize)> = Vec::with_capacity(8);
+    if let Some(i) = line.find("one") {
+        found.push((i, 1))
+    };
 }
